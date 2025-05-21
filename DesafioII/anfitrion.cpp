ac14feb9ca86b71c7002fa39e70a6555415cd957
Miche <111651543+estemiche@users.noreply.github.com>
2025-05-21 07:53:24 -0500

    Add files via upload
    Implementación de la clase anfitrion

---
+++ DesafioII/anfitrion.cpp
@@ -0,0 +1,44 @@
+#include "Anfitrion.h"
+#include <iostream>
+
+Anfitrion::Anfitrion(const char* doc, int ant, float punt)
+    : Usuario(doc, ant, punt) {
+    capacidad = 3;
+    totalAlojamientos = 0;
+    alojamientos = new Alojamiento*[capacidad];
+}
+
+Anfitrion::~Anfitrion() {
+    for (int i = 0; i < totalAlojamientos; i++) {
+        delete alojamientos[i];
+    }
+    delete[] alojamientos;
+}
+
+void Anfitrion::expandirAlojamientos() {
+    capacidad *= 2;
+    Alojamiento** nuevo = new Alojamiento*[capacidad];
+    for (int i = 0; i < totalAlojamientos; i++) {
+        nuevo[i] = alojamientos[i];
+    }
+    delete[] alojamientos;
+    alojamientos = nuevo;
+}
+
+void Anfitrion::agregarAlojamiento(Alojamiento* a) {
+    if (totalAlojamientos == capacidad) {
+        expandirAlojamientos();
+    }
+    alojamientos[totalAlojamientos++] = a;
+}
+
+void Anfitrion::consultarReservasActivas() const {
+    for (int i = 0; i < totalAlojamientos; i++) {
+        alojamientos[i]->mostrarReservas(); // asumimos que esta función existe en Alojamiento
+    }
+}
+
+void Anfitrion::mostrarDatos() const {
+    std::cout << "Anfitrion: " << documento << " | Antigüedad: " << antiguedad
+              << " meses | Puntuación: " << puntuacion << std::endl;
+}
--- DesafioII/anfitrion.h
+++ DesafioII/anfitrion.h
@@ -1,4 +1,26 @@
 #ifndef ANFITRION_H
 #define ANFITRION_H
 
-#endif // ANFITRION_H
+#include "Usuario.h"
+#include "Alojamiento.h"
+
+class Anfitrion : public Usuario {
+private:
+    Alojamiento** alojamientos;
+    int totalAlojamientos;
+    int capacidad;
+
+    void expandirAlojamientos();
+
+public:
+    Anfitrion(const char* doc = "", int ant = 0, float punt = 0.0f);
+    ~Anfitrion();
+
+    void agregarAlojamiento(Alojamiento* a);
+    void consultarReservasActivas() const;
+
+    void mostrarDatos() const override;
+};
+
+#endif
+
